3ab82f03c87475b9df794fce09bebfb46ffa7eaf
Andrey Masiero <andreymasiero@gmail.com>
2017-09-11 22:00:17 -0300

    Resolução BubbleSort e Buscas

--- aula_04/src/com/andreymasiero/aulas/algoritmos/Search.java
+++ aula_04/src/com/andreymasiero/aulas/algoritmos/Search.java
@@ -1,5 +1,29 @@
 package com.andreymasiero.aulas.algoritmos;
 
 public class Search {
-	// CODIGO FONTE AQUI
+	
+	public static int sequentialSearch(int[] data, int value) {		
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] == value) {
+				return i;
+			}
+		}		
+		return -1;
+	}
+	
+	public static int binarySearch(int[] data, int value) {
+		int begin = 0;
+		int end = data.length - 1;
+		
+		while (begin < end) {
+			int middle = (begin + end) / 2;
+			if (data[middle] > value)
+				end = middle - 1;
+			else if (data[middle] < value)
+				begin = middle + 1;
+			else
+				return middle;
+		}
+		return -1;
+	}
 }
--- aula_04/src/com/andreymasiero/aulas/algoritmos/Sort.java
+++ aula_04/src/com/andreymasiero/aulas/algoritmos/Sort.java
@@ -1,5 +1,17 @@
 package com.andreymasiero.aulas.algoritmos;
 
 public class Sort {
-    // CODIGO FONTE AQUI
+     
+	public static void bubblesort (int[] data) {
+		for (int i = 0; i < data.length; i++) 
+			for (int j = 0; j < data.length - 1; j++)
+				if (data[j] > data[j + 1])
+					swap(data, j, j + 1);
+	}
+	
+	public static void swap(int[] d, int a, int b) {
+		int aux = d[a];
+		d[a] = d[b];
+		d[b] = aux;
+	}
 }
